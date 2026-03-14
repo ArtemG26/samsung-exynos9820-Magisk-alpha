3608a66d5d4efd2616a350be55f16f9880db1775
ArtemG26 <128422366+ArtemG26@users.noreply.github.com>
2026-03-15 00:01:35 +0200

    Update update_magisk.sh

--- usr/magisk/update_magisk.sh
+++ usr/magisk/update_magisk.sh
@@ -13,7 +13,7 @@ then
 elif [ "x$1" = "xalpha" ]
 then
 	nver="alpha"
-	magisk_link="https://github.com/vvb2060/Magisk/releases/latest/download/app-release.apk"
+	magisk_link="https://github.com/vvb2060/Magisk/releases/download/30700/app-release.apk"
 else
 	dash='-'
 	if [ "x$1" = "x" ]; then
